3d68a94cdf0d660788ab9f0b7e9bf43e84418642
Harvey Gomez <hmgomez90@gmail.com>
2019-05-17 16:50:00 +0100

    Fix ESLint issues on repoHandler

--- repoHandler.js
+++ repoHandler.js
@@ -1,21 +1,21 @@
 /** TODO
  * Get number of commits per repository
  * Get number of forks a users has against their repositories
- * 
  */
 
 const github = require('./api/github');
+
 const repos = github.getRepositoriesForUser('zcoinofficial');
 console.log(repos);
 
 /**
  * Takes an array of repository objects for a user and returns the total number
- * of stars for that dataset. 
- * @param {array} repositories 
+ * of stars for that dataset.
+ * @param {array} repositories
  */
 function usersTotalStars(repositories) {
-    initialValue = 0;
-    const stars = repositories.reduce(function(accumulator, currentValue) {
+    const initialValue = 0;
+    const stars = repositories.reduce((accumulator, currentValue) => {
         return accumulator + currentValue.stargazers_count;
     }, initialValue);
 
@@ -27,8 +27,8 @@ function usersTotalCommits(){
 }
 
 function usersTotalWatchers() {
-    initialValue = 0;
-    const watchers = repositories.reduce(function(accumulator, currentValue) {
+    const initialValue = 0;
+    const watchers = repositories.reduce((accumulator, currentValue) => {
         return accumulator + currentValue.watchers_count;
     }, initialValue);
 
