a40462d9158102e4e6ea9a473e81ebfc5612192b
Harvey Gomez <hmgomez90@gmail.com>
2019-05-19 02:25:28 +0100

    Add test for gist and filter.

--- test/github.test.js
+++ test/github.test.js
@@ -1,10 +1,20 @@
 const github = require('../api/github');
 
 describe('Github API communications', () => {
-    it('should load user repositories', () => {
+    test('should get a gist from github API', () => {
+        return github.getGist('137c9c481c8c5756519f991ade64253a').then(data => {
+            expect(data).toBeDefined();
+        })
+    });
+
+    test('should load user repositories', () => {
         return github.getRepositoriesForUser('h-gomez').then(data => {
             expect(data).toBeDefined();
-            expect(data.message).toUndefined();
+            expect(data.message).toBeUndefined();
         });
+    });
+
+    test('should filter username from github url', () => {
+        expect(github.filterUsernameFromRepo('https://github.com/H-Gomez/crypto-commits-runner')).toEqual('H-Gomez');
     });
 });
